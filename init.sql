8c3459a5f61e6054e50c08fe9a0dfd046cc4cd4f
romesdev <fs.romes@gmail.com>
2024-11-14 11:34:54 -0300

    feat: add user and post entities

--- init.sql
+++ init.sql
@@ -1,5 +1,15 @@
 USE test_db;
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
---TODO Crie a tabela de user;
-
---TODO Crie a tabela de posts;
+CREATE TABLE post (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
