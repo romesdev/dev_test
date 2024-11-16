ea11b3ea8a6c2e932d1ada08c30c336b3aa47b45
romesdev <fs.romes@gmail.com>
2024-11-16 17:54:51 -0300

    refactor: fix typing in validationMiddleware

--- src/middleware/validationMiddleware.ts
+++ src/middleware/validationMiddleware.ts
@@ -1,27 +1,27 @@
-import { Request, Response, NextFunction } from 'express';
-import { z, ZodError } from 'zod';
+import { Request, Response, NextFunction } from "express";
+import { ZodError, ZodObject, ZodRawShape } from "zod";
 import {
   BAD_REQUEST_STATUS_CODE,
   INTERNAL_SERVER_ERROR_STATUS_CODE,
-} from '../utils/constants';
+} from "../utils/constants";
 
-export function validateData(schema: z.ZodObject<any, any>) {
+export function validateData(schema: ZodObject<ZodRawShape>) {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
       schema.parse(req.body);
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        const errorMessages = error.errors.map((issue: any) => ({
-          message: `${issue.path.join('.')} is ${issue.message}`,
+        const errorMessages = error.errors.map((issue) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
         }));
         res
           .status(BAD_REQUEST_STATUS_CODE)
-          .json({ error: 'Invalid data', details: errorMessages });
+          .json({ error: "Invalid data", details: errorMessages });
       } else {
         res
           .status(INTERNAL_SERVER_ERROR_STATUS_CODE)
-          .json({ error: 'Internal Server Error' });
+          .json({ error: "Internal Server Error" });
       }
     }
   };
